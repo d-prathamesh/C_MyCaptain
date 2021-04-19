3ccb123c032eb265066b658fd5a3a129a6f6a702
Prathamesh Deokar <69900104+d-prathamesh@users.noreply.github.com>
2021-04-19 16:16:27 +0530

    Create Marksheet.c

---
+++ if_else/Marksheet.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+    printf("Enter your marks ");
+    scanf("%d",&marks);
+    if(marks<0 || marks>100)
+    {
+        printf("Please enter marks from 0 to 100");
+    }
+    else if(marks<40)
+    {
+        printf("Grade F");
+    }
+    else if(marks>=40 && marks<54)
+    {
+        printf("Grade D");
+    }
+    else if(marks>=55 && marks<69)
+    {
+        printf("Grade C");
+    }
+    else if(marks>=70 && marks<84)
+    {
+        printf("Grade B");
+    }
+    else if(marks>=84 && marks<= 100)
+    {
+        printf("Grade A");
+    }
+    
+		return 0;
+}
